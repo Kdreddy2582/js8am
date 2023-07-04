e12e8acd7a9d82d3b89e7716ca5e171bee886108
Devender Reddy <41923640+Kdreddy2582@users.noreply.github.com>
2023-07-04 07:41:03 +0530

    JavaScript classes
    JavaScript classes

---
+++ variables.js
@@ -0,0 +1,38 @@
+// var firstNname = "Deveder";
+// console.log(firstNname);
+// firstNname = "Deveder Reddy";
+// console.log(firstNname);
+// firstNname = "Deveder Reddy 1234";
+// console.log(firstNname);
+
+var firstNname = "Devender";
+document.write(firstNname);
+document.write("<br>")
+
+var age = 50;
+document.write(age);
+document.write("<br>")
+
+let lastName = "Kagital"
+document.write(lastName)
+
+
+
+
+
+// let lastName = "<h1>Kagitala</h1>";
+// document.write(lastName);
+// lastName = "<h1>Kagitala 123</h1>";
+// document.write(lastName);
+
+
+// const age = 100;
+// document.write(age)
+// age = 90
+
+// const age ="";
+// document.write(age)
+
+
+
+
